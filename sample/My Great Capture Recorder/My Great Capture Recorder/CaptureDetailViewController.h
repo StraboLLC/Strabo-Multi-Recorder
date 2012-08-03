8ec03f318bda4f1dfd59221b0f5cd08e902f44fe
Nate Beatty <nate.beatty@gmail.com>
2012-08-03 14:10:09 -0400

    Upload delegation temp fix

--- sample/My Great Capture Recorder/My Great Capture Recorder/CaptureDetailViewController.h
+++ sample/My Great Capture Recorder/My Great Capture Recorder/CaptureDetailViewController.h
@@ -11,13 +11,24 @@
 
 @interface CaptureDetailViewController : UIViewController {
     // UI Elements
-    IBOutlet UILabel * titleLabel;
-    IBOutlet UILabel * tokenLabel;
+    IBOutlet UITextField * titleField;
+    IBOutlet UILabel * dateLabel;
+    IBOutlet UILabel * uploadSuccessLabel;
+    IBOutlet UIButton * uploadButton;
+    IBOutlet UIProgressView * progressView;
     IBOutlet UIImageView * thumbnailImage;
     
     STRCapture * _localCapture;
 }
 
+/**
+ The STRCapture object associated with this detail view.
+ */
 @property(nonatomic, strong)STRCapture * localCapture;
+
+/**
+ An action called when the upload button is pressed.
+ */
+-(IBAction)uploadButtonWasPressed:(id)sender;
 
 @end
--- sample/My Great Capture Recorder/My Great Capture Recorder/CaptureDetailViewController.m
+++ sample/My Great Capture Recorder/My Great Capture Recorder/CaptureDetailViewController.m
@@ -8,7 +8,32 @@
 
 #import "CaptureDetailViewController.h"
 
-@interface CaptureDetailViewController ()
+@interface CaptureDetailViewController (STRCaptureUploadManagerDelegate) <STRCaptureUploadManagerDelegate>
+
+-(void)fileUploadDidStart;
+-(void)fileUploadFailedToStart;
+-(void)fileUploadDidProgress:(NSNumber *)progress;
+-(void)fileUploadedSuccessfullyWithToken:(NSString *)token;
+-(void)fileUploadDidStop;
+-(void)fileUploadDidFailWithError:(NSError *)error;
+
+@end
+
+@interface CaptureDetailViewController (UITextField)
+
+@end
+
+@interface CaptureDetailViewController (InternalMethods)
+
+/**
+ Updates the user interface elements uploadButton and uploadSuccessLabel to reflect current upload status.
+ */
+-(void)refreshCaptureDetail;
+
+/**
+ Called to upload the _localCapture to the server using a STRCaptureUploadManager
+ */
+-(void)uploadCapture;
 
 @end
 
@@ -28,16 +53,103 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
-    titleLabel.text = _localCapture.title;
-    tokenLabel.text = _localCapture.token;
-    thumbnailImage.image = _localCapture.thumbnailImage;
+}
+
+-(void)viewWillAppear:(BOOL)animated {
+    [self refreshCaptureDetail];
+}
+
+-(void)viewWillDisappear:(BOOL)animated {
+    // Save any changes made to the capture object
+    [_localCapture save];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Button Handling
+
+-(IBAction)uploadButtonWasPressed:(id)sender {
+    // Upload the cature associated with this detail view
+    [self uploadCapture];
+}
+
+@end
+
+@implementation CaptureDetailViewController (STRCaptureUploadManagerDelegate)
+
+-(void)fileUploadDidStart {
+    NSLog(@"File upload did start.");
+    // Show the progress view and reset the value to 0%
+    [progressView setProgress:0.0 animated:NO];
+    [progressView setHidden:NO];
+}
+
+-(void)fileUploadFailedToStart {
+    [self refreshCaptureDetail];
+    NSLog(@"File upload failed to start.");
+}
+
+-(void)fileUploadDidProgress:(NSNumber *)progress {
+    // Update the progress view
+    [progressView setProgress:[progress floatValue] animated:YES];
+}
+
+-(void)fileUploadedSuccessfullyWithToken:(NSString *)token {
+    // Make sure the progress view reads 100%
+    [progressView setProgress:1.0 animated:YES];
+    // Save the upload date to the capture
+    _localCapture.uploadDate = [NSDate date];
+    // Refresh the user interface
+    [self refreshCaptureDetail];
+    NSLog(@"File uploaded successfully.");
+}
+
+-(void)fileUploadDidStop {
+    [self refreshCaptureDetail];
+    NSLog(@"File upload did stop");
+}
+
+-(void)fileUploadDidFailWithError:(NSError *)error {
+    [self refreshCaptureDetail];
+    NSLog(@"File upload failed: %@", error.localizedDescription);
+}
+
+@end
+
+@implementation CaptureDetailViewController (InternalMethods)
+
+-(void)refreshCaptureDetail {
+    if (!_localCapture.uploadDate) {
+        // Hide the upload success label if the capture has not been uploaded.
+        [uploadSuccessLabel setHidden:YES];
+        [uploadButton setEnabled:YES];
+    } else {
+        // Show the upload label, as the capture has previously been uploaded.
+        [uploadSuccessLabel setHidden:NO];
+        [uploadButton setEnabled:NO];
+    }
+    // Either way, hide the progress view
+    [progressView setHidden:YES];
+    
+    // Set up a date formatter to beautify the NSDate from the capture object
+    NSDateFormatter * dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"MM/dd/yyyy 'at' hh:mm a"];
+    NSString * captureCreationDateString = [dateFormatter stringFromDate:_localCapture.creationDate];
+    
+    // Populate the view with the information from the capture
+    titleField.text = _localCapture.title;
+    dateLabel.text = captureCreationDateString;
+    thumbnailImage.image = _localCapture.thumbnailImage;
+}
+
+-(void)uploadCapture {
+    STRCaptureUploadManager * uploadManager = [STRCaptureUploadManager defaultManager];
+    uploadManager.delegate = self;
+    [uploadManager beginUploadForCapture:_localCapture];
 }
 
 @end
